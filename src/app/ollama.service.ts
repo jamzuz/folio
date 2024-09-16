19f26e5fc590cbfb419aab08ba1e607ba3be0f78
jamzuz <jesseromo3@hotmail.com>
2024-09-16 15:06:33 +0300

    cleaned up ollama service. cleaned up frontpage

--- src/app/ollama.service.ts
+++ src/app/ollama.service.ts
@@ -1,30 +1,30 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 
 export type llamaResponse = {
-  response : string
+  response: string
+  context: Array<string>
 }
 
-
-@Injectable({
-  providedIn: 'root'
-})
 export class OllamaService {
-  
-  llamaChat : string = "dafasdfsa";
+  // beautiful debug is beautiful
+  llamaChat: string = "dafasdfsa";
+  firstMessage: boolean = true;
 
-  constructor(private http:HttpClient) { 
-    
-   }
+  constructor(private http: HttpClient) {
 
-  async talkToLlama(message:string){
+  }
+
+  async talkToLlama(message: string) {
     const body = {
       "stream": false,
       "model": "llama3.1",
-      "prompt": message.toString()
+      "prompt": ""
     }
+    body.prompt = message.toString()
+
     const t = await lastValueFrom(this.http.post<llamaResponse>('http://localhost:11434/api/generate', body))
     this.llamaChat = t.response
+    console.log(t)
   }
 }

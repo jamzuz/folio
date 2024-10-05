147895ef830854debc81944085797091332cbff5
Jesse Romo <jesseromo3@hotmail.com>
2024-10-05 16:01:59 +0300

    Created local storage service. Refactored old local storage handling. Cleaned up code.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,6 +3,7 @@ import { Router, RouterLink } from '@angular/router';
 import { OllamaService } from '../services/ollama.service';
 import { NgIf } from '@angular/common';
 import { ButtonComponent } from '../button/button.component';
+import { LocalStorageService } from '../services/local-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -13,7 +14,7 @@ import { ButtonComponent } from '../button/button.component';
 })
 export class HeaderComponent {
 
-  constructor(private ollama: OllamaService, private router: Router) { }
+  constructor(private localStorageService: LocalStorageService, private router: Router) { }
 
   currentUrl(): string {
     return this.router.url
@@ -22,7 +23,7 @@ export class HeaderComponent {
   reset() {
     const alert = confirm("This will erase all chat logs and restart the whole process, continue?")
     if (alert == true) {
-      this.ollama.clearLocalStorage()
+      this.localStorageService.clearLocalStorage()
     }
   }
 }
